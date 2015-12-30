655aa43f7e50a5cb96ee8486258d4217d89ea598
Matt Spaulding <mrmattspaulding@gmail.com>
2015-12-30 11:57:33 -0500

    Sidenav now repeats items from model.

--- app/controllers/indexCtrl.js
+++ app/controllers/indexCtrl.js
@@ -1,11 +1,24 @@
 ﻿'use strict';
 app.controller('IndexCtrl', function ($scope, $mdSidenav, $state) {
-    
-    $scope.title = 'HOME';
+    $scope.menuItems = [
+      { name: 'autocomplete', path: 'autocomplete' },
+      { name: 'bottom sheet', path: 'bottomSheet' },
+      { name: 'button', path: 'button' },
+      { name: 'card', path: 'card' },
+      { name: 'date picker', path: 'datePicker' },
+      { name: 'grid list', path: 'gridList' },
+      { name: 'input', path: 'input' },
+      { name: 'progress circular', path: 'progressCircular' },
+      { name: 'progress linear', path: 'progressLinear' },
+      { name: 'toast', path: 'toast' },
+      { name: 'whiteframe', path: 'whiteframe' },
+    ];
 
-    $scope.go = function (path) {
+    $scope.title = 'home';
+
+    $scope.go = function (path, title) {
         $state.go(path);
-        
+        $scope.title = title;
     }
 
     $scope.toggleLeft = function () {
