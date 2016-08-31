af3079bf80163fcee03b2e6f54b214c2b34fa545
Matt Spaulding <mrmattspaulding@gmail.com>
2016-08-31 00:59:02 -0400

    Fix gulp inject.

--- gulp/inject.js
+++ gulp/inject.js
@@ -22,7 +22,7 @@ gulp.task('inject', ['scripts', 'styles'], function () {
   ], { read: false });
 
   var injectScripts = gulp.src([
-    path.join(conf.paths.src, '/app/**/*.module.js'),
+    path.join(conf.paths.src, '/app/**/*.controller.js'),
     path.join(conf.paths.src, '/app/**/*.js'),
     path.join('!' + conf.paths.src, '/app/**/*.spec.js'),
     path.join('!' + conf.paths.src, '/app/**/*.mock.js'),
