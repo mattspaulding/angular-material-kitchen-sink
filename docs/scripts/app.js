4f0d1b6904bc34ccb1a704e10c5dfddecd6d6a7f
Matt Spaulding <matthew.spaulding@iprocedures.com>
2017-03-08 19:06:25 -0500

    Fix for icon dependency.

--- docs/scripts/app.js
+++ docs/scripts/app.js
@@ -252,22 +252,6 @@
 
 })();
 
-(function() {
-  'use strict';
-
-  angular
-    .module('angularMaterialKitchenSink')
-    .config(iconConfig);
-
-  /** @ngInject */
-  function iconConfig($mdIconProvider) {
-    $mdIconProvider
-      .iconSet('social', 'bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-social.svg', 24)
-      .defaultIconSet('img/icons/sets/core-icons.svg', 24);
-  }
-
-})();
-
 /* global malarkey:false, moment:false */
 (function() {
   'use strict';
